76e2e6de7930c3fec2ddde114d1ebf324b45a6e6
Aqueel Miqdad <amiqdad@hawk.iit.edu>
2017-05-17 14:36:05 -0500

    Retrain Tenserflow modified added

--- app.py
+++ app.py
@@ -18,6 +18,56 @@ def homepage():
 
     return "Hello"
 
+@app.route('/predictfile', methods=['POST'])
+def predict2():
+
+    file = request.files['file']
+    filename = 'tests.jpg'
+    file.save(os.path.join(APP_ROOT, filename))
+
+    image_path = os.path.join(APP_ROOT, 'tests.jpg')
+
+    image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+
+    labels = os.path.join(APP_ROOT, "retrained_labels.txt")
+
+    label_lines = [line.rstrip() for line
+                   in tf.gfile.GFile(labels)]
+
+    models = os.path.join(APP_ROOT, "retrained_graph.pb")
+
+    with tf.gfile.FastGFile(models, 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        tf.import_graph_def(graph_def, name='')
+
+    with tf.Session() as sess:
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+
+        predictions = sess.run(softmax_tensor, \
+                               {'DecodeJpeg/contents:0': image_data})
+
+        # Sort to show labels of first prediction in order of confidence
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+
+        winner = label_lines[top_k[0]]
+
+        diff12 = abs(predictions[0][top_k[0]] - predictions[0][top_k[1]])
+        diff23 = abs(predictions[0][top_k[1]] - predictions[0][top_k[2]])
+        diff34 = abs(predictions[0][top_k[2]] - predictions[0][top_k[3]])
+
+        if(diff12 < 0.2):
+            winner = 'confused'
+
+        for node_id in top_k:
+            human_string = label_lines[node_id]
+            score = predictions[0][node_id]
+            print('%s (score = %.5f)' % (human_string, score))
+
+    os.remove(image_path)
+
+    return jsonify({'winner': winner})
+
 @app.route('/predict', methods=['POST'])
 def predict():
 
